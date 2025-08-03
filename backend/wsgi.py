3859ebedab91deea6ccff3daeb859c61c6f441b5
muskandz <muskandadhich14@gmail.com>
2025-08-03 15:37:27 +0530

    fixed railway

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -19,4 +19,4 @@ if __name__ == "__main__":
                 debug=False, 
                 host="0.0.0.0", 
                 port=port,
-                allow_unsafe_werkzeug=True)
+                use_reloader=False)
